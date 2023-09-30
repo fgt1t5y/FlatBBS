d97d85129640f657c22a21c8b6d00c3cbb4ebe5c
望 <1501034130@qq.com>
2023-09-30 12:19:07 +0800

    feat(frontend): 用户菜单雏形

--- site/src/components/Navbar.vue
+++ site/src/components/Navbar.vue
@@ -18,9 +18,40 @@
             <kbd>/</kbd>
           </template>
         </Input>
-        <Popover v-if="isLogin" position="br" trigger="click">
-          <Avatar :size="32" :image-url="user.info.avatar_uri" />
-          <template #content>欢迎你，{{ user.info.username }} </template>
+        <Popover
+          v-if="isLogin"
+          :popup-visible="userMenuShow"
+          position="br"
+          trigger="click"
+          content-class="user-menu"
+        >
+          <Avatar
+            :size="32"
+            :image-url="user.info.avatar_uri"
+            @click="userMenuShow = true"
+          />
+          <template #content>
+            <header>
+              <Avatar :size="56" :image-url="user.info.avatar_uri" />
+              <div class="user-menu-info">
+                <TypographyTitle :heading="5">{{
+                  user.info.username
+                }}</TypographyTitle>
+                <TypographyText type="secondary">{{
+                  user.info.email
+                }}</TypographyText>
+              </div>
+            </header>
+            <Divider />
+            <main>
+              <Doption
+                v-for="options in userMenuOptions"
+                @click="options.action"
+              >
+                <div>{{ options.text }}</div>
+              </Doption>
+            </main>
+          </template>
         </Popover>
         <RouterLink to="/auth" v-else>
           <Button type="primary">注册 / 登录</Button>
@@ -34,11 +65,14 @@
 import { useUserStore } from "@/stores";
 import {
   TypographyTitle,
+  TypographyText,
   Input,
   Button,
   Space,
   Avatar,
   Popover,
+  Doption,
+  Divider,
 } from "@arco-design/web-vue";
 import { IconSearch } from "@arco-design/web-vue/es/icon";
 import { onMounted, ref } from "vue";
@@ -65,11 +99,30 @@ interface NavbarProps {
   isLogin: boolean;
 }
 
+interface UserMenuOptionsProps {
+  text: string;
+  action: (ev?: MouseEvent) => void;
+}
+
 const props = withDefaults(defineProps<NavbarProps>(), {
   isLogin: false,
 });
 
 const user = useUserStore();
+const userMenuShow = ref<boolean>(false);
+
+const userMenuOptions = [
+  {
+    text: "设置",
+    action: () => {
+      console.log("click");
+      userMenuShow.value = !userMenuShow.value;
+    },
+  },
+  {
+    text: "退出登录",
+  },
+] as UserMenuOptionsProps[];
 </script>
 
 <style>
@@ -97,6 +150,37 @@ const user = useUserStore();
 }
 
 .nav-brand h5 {
+  margin: unset;
+}
+
+.user-menu {
+  min-width: 300px;
+  max-width: 40vw;
+  padding: 8px !important;
+}
+
+.user-menu .arco-avatar {
+  margin-right: 0.7rem;
+}
+
+.user-menu header {
+  display: flex;
+  padding: 0px 12px;
+}
+
+.user-menu main {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.user-menu-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.user-menu-info h5 {
   margin: unset;
 }
 </style>
