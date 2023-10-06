dfc35ef98a9c1a2af67d41dd0ca0ae1c32bf8042
望 <1501034130@qq.com>
2023-10-06 19:07:20 +0800

    chore: 重构settings页面

--- site/src/views/SettingsView.vue
+++ site/src/views/SettingsView.vue
@@ -4,14 +4,6 @@
     <template #content>
       <div class="page-settings">
         <TypographyTitle :heading="3">设置</TypographyTitle>
-        <TypographyText
-          >此页面用于调整用户名、密码与主题颜色等设置。
-        </TypographyText>
-        <Divider :margin="12" />
-        <fieldset>
-          <legend>我的资料</legend>
-          <TypographyTitle :heading="5">我的资料</TypographyTitle>
-        </fieldset>
       </div>
     </template>
   </CommonGrid>
@@ -19,7 +11,12 @@
 
 <script setup lang="ts">
 import CommonGrid from "@/components/CommonGrid.vue";
-import { TypographyTitle, TypographyText, Divider } from "@arco-design/web-vue";
+import {
+  TypographyTitle,
+  TypographyText,
+  Divider,
+  Button,
+} from "@arco-design/web-vue";
 </script>
 
 <style>
