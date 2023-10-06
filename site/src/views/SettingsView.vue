fd598ef5bf5886f14ec10bc87086395d78f9f930
望 <1501034130@qq.com>
2023-10-06 18:26:41 +0800

    chore: add eslint

--- site/src/views/SettingsView.vue
+++ site/src/views/SettingsView.vue
@@ -1,5 +1,16 @@
-<template></template>
+<template>
+  <CommonGrid>
+    <template #sider>g2</template>
+    <template #content>
+      <TypographyTitle :heading="3">设置</TypographyTitle>
+      <Divider />
+    </template>
+  </CommonGrid>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CommonGrid from "@/components/CommonGrid.vue";
+import { TypographyTitle, Divider } from "@arco-design/web-vue";
+</script>
 
 <style></style>
