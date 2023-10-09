b35ee5fe58df2dcaebcec9146bff3cab844c3dfe
望 <1501034130@qq.com>
2023-10-09 12:03:20 +0800

    chore: 设置 vite 代理及跨域

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,19 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   server: {
-    port: 3901
-  }
-})
+    port: 3901,
+    proxy: {
+      '/backend': {
+        target: 'http://192.168.1.108:3900',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/backend/, ''),
+      },
+    },
+  },
+});
