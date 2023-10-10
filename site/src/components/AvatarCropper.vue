60b26d8aba297b2ef2093613ce5f0dc64d409bc7
望 <1501034130@qq.com>
2023-10-10 14:59:26 +0800

    feat: 头像裁剪框防止超出画布

--- site/src/components/AvatarCropper.vue
+++ site/src/components/AvatarCropper.vue
@@ -7,18 +7,13 @@
       @mousemove="mousemove"
       @mouseup="mouseup"
     >
-      <img
-        ref="imageSrc"
-        src="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/6480dbc69be1b5de95010289787d64f1.png~tplv-uwbnlip3yd-webp.webp"
-        alt="src"
-        style="display: none"
-      />
+      <img ref="imageSrc" src="" alt="src" style="display: none" />
       <canvas ref="canvasRef" :width="size" :height="size"></canvas>
       <div class="cropper-wrapper">
         <div class="cropper-mask"></div>
       </div>
     </div>
-    <Slider :min="1.0" :max="2.0" :step="0.01" />
+    <Slider v-model="scale" :min="1.0" :max="2.0" :step="0.01" />
   </section>
 </template>
 
@@ -52,8 +47,70 @@ const renderStatus = {
 const scale = ref<number>(1.0)
 const imageSizeException = ref<boolean>(false)
 let ctx: CanvasRenderingContext2D | null = null
+let borderDistanceX = 0
+let borderDistanceY = 0
+const renderErrorMessage = (message: string) => {
+  imageSizeException.value = true
+  ctx!.font = 'bold 16px serif'
+  ctx!.fillStyle = 'red'
+  ctx!.fillText(message, 64, 128)
+}
 const initCanvas = () => {
-  ctx?.drawImage(imageSrc.value!, 0, 0, 400, 400, 0, 0, 400, 400)
+  imageSrc.value!.src =
+    'https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/6480dbc69be1b5de95010289787d64f1.png~tplv-uwbnlip3yd-webp.webp'
+  imageSrc.value!.onload = () => {
+    if (
+      imageSrc.value!.width < props.size ||
+      imageSrc.value!.height < props.size
+    ) {
+      renderErrorMessage(`图像高宽至少为 ${props.size}`)
+      return
+    }
+  }
+  imageSrc.value!.onerror = () => {
+    renderErrorMessage('渲染错误：没有图像源。')
+    return
+  }
+  borderDistanceX = imageSrc.value!.width - props.size
+  borderDistanceY = imageSrc.value!.height - props.size
+  ctx?.drawImage(
+    imageSrc.value!,
+    0,
+    0,
+    imageSrc.value!.width,
+    imageSrc.value!.height,
+  )
+}
+const drawAt = (x: number, y: number) => {
+  ctx?.drawImage(
+    imageSrc.value!,
+    x,
+    y,
+    imageSrc.value!.width,
+    imageSrc.value!.height,
+  )
+}
+const draw = () => {
+  ctx?.clearRect(0, 0, props.size, props.size)
+  const currentX = renderStatus.clientX + renderStatus.deltaX
+  const currentY = renderStatus.clientY + renderStatus.deltaY
+
+  drawAt(currentX, currentY)
+}
+const checkOverBorder = () => {
+  if (renderStatus.clientX > 0) {
+    renderStatus.clientX = 0
+  }
+  if (renderStatus.clientX < -borderDistanceX) {
+    renderStatus.clientX = -borderDistanceX
+  }
+  if (renderStatus.clientY > 0) {
+    renderStatus.clientY = 0
+  }
+  if (renderStatus.clientY < -borderDistanceY) {
+    renderStatus.clientY = -borderDistanceY
+  }
+  drawAt(renderStatus.clientX, renderStatus.clientY)
 }
 const mousedown = (ev: MouseEvent) => {
   if (imageSizeException.value) return
@@ -62,7 +119,6 @@ const mousedown = (ev: MouseEvent) => {
   renderStatus.isDraging = true
   renderStatus.startX = clientX
   renderStatus.startY = clientY
-  console.error(renderStatus)
 }
 const mousemove = (ev: MouseEvent) => {
   if (!renderStatus.isDraging) return
@@ -81,35 +137,12 @@ const mouseup = (ev: MouseEvent) => {
   renderStatus.clientY += deltaY
   renderStatus.deltaX = 0
   renderStatus.deltaY = 0
-}
-const draw = () => {
-  ctx?.clearRect(0, 0, props.size, props.size)
-  const currentX = renderStatus.clientX + renderStatus.deltaX
-  const currentY = renderStatus.clientY + renderStatus.deltaY
-
-  ctx?.drawImage(
-    imageSrc.value!,
-    currentX,
-    currentY,
-    imageSrc.value!.width,
-    imageSrc.value!.height,
-  )
-
-  console.log(renderStatus)
+  console.error(renderStatus)
+  checkOverBorder()
 }
 
 onMounted(() => {
   ctx = canvasRef.value!.getContext('2d')
-  if (
-    imageSrc.value!.width < props.size ||
-    imageSrc.value!.height < props.size
-  ) {
-    imageSizeException.value = true
-    ctx!.font = 'bold 24px serif'
-    ctx!.fillStyle = 'red'
-    ctx!.fillText(`图像高宽至少为 ${props.size}`, 0, 128)
-    return
-  }
   initCanvas()
 })
 </script>
