3a582194bf5c9bba76024e39e4433a5f0ac0398c
望 <1501034130@qq.com>
2023-10-12 21:49:52 +0800

    feat: 限制上传文件类型&防止非图片被上传

--- app/controller/FileController.php
+++ app/controller/FileController.php
@@ -2,12 +2,13 @@
 
 namespace app\controller;
 
+use Intervention\Image\Exception\NotReadableException;
 use support\Request;
 use Intervention\Image\ImageManagerStatic as image;
 
 class FileController
 {
-  static $supportedFile = ['image/png'];
+  static $supportedFile = ['image/png', 'image/jpeg'];
 
   public function index(Request $request)
   {
@@ -18,13 +19,22 @@ class FileController
   public function upload(Request $request)
   {
     $file = $request->file('avgfile');
-    $filename = random_string() . '.jpg';
 
-    if (!$file || !$file->isValid()) {
+    if (
+      !$file
+      || !$file->isValid()
+      || !in_array($file->getUploadMimeType(), self::$supportedFile)
+    ) {
       return json_message(STATUS_BAD_REQUEST, '无效文件');
     }
 
-    $image = image::make($file->getPathname());
+    $filename = random_string() . '.jpg';
+
+    try {
+      $image = image::make($file->getPathname());
+    } catch (NotReadableException $e) {
+      return json_message(STATUS_BAD_REQUEST, '损坏的文件');
+    }
     $image->save("public/usercontent/{$filename}", 60, 'jpg');
 
     return json_message(STATUS_OK, '完成');
