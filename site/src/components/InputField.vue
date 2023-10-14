0649cea1b5d9c41013936cebca00fd04fb3e2176
望 <1501034130@qq.com>
2023-10-14 22:43:56 +0800

    chore: 美化设置项的样式

--- site/src/components/InputField.vue
+++ site/src/components/InputField.vue
@@ -1,22 +1,24 @@
 <template>
-  <Space>
-    <Input
-      v-model="valueNow"
-      :readonly="readonly"
-      :max-length="maxLength"
-      @focus="onFocus"
-    />
-    <div v-if="showOptions" class="input-field-opt">
+  <Space v-if="isEditing">
+    <Input v-model="valueNow" :max-length="maxLength" />
+    <div class="input-field-opt">
       <Button type="primary" @click="onConfirm">保存</Button>
       <Button @click="onCancle">取消</Button>
     </div>
   </Space>
+  <Space v-else>
+    <TypographyText>{{ valueNow }}</TypographyText>
+    <button class="icon-link" title="更改此栏" @click="startEdit">
+      <IconEdit />
+    </button>
+  </Space>
 </template>
 
 <script setup lang="ts">
-import { Input, Button, Space } from '@arco-design/web-vue'
+import { Input, Button, Space, TypographyText } from '@arco-design/web-vue'
 import { ref, watch } from 'vue'
 import './InputField.css'
+import { IconEdit } from '@arco-design/web-vue/es/icon'
 
 defineOptions({
   name: 'InputField',
@@ -25,7 +27,6 @@ defineOptions({
 interface InputFieldProps {
   inputValue: string
   maxLength?: number
-  readonly?: boolean
 }
 
 const props = withDefaults(defineProps<InputFieldProps>(), {
@@ -33,20 +34,20 @@ const props = withDefaults(defineProps<InputFieldProps>(), {
   maxLength: 32,
 })
 const valueNow = ref<string>(props.inputValue)
-const showOptions = ref<boolean>(false)
+const isEditing = ref<boolean>(false)
 const onConfirm = () => {
   if (valueNow.value.trim() === '' || valueNow.value === props.inputValue) {
     valueNow.value = props.inputValue
-    showOptions.value = false
+    isEditing.value = false
     return
   }
 }
 const onCancle = () => {
   valueNow.value = props.inputValue
-  showOptions.value = false
+  isEditing.value = false
 }
-const onFocus = () => {
-  if (!props.readonly) showOptions.value = true
+const startEdit = () => {
+  isEditing.value = true
 }
 
 watch(
