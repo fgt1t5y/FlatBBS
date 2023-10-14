a36b9936589aa42465da4c1bf51cb6eea4d4435a
望 <1501034130@qq.com>
2023-10-14 23:03:15 +0800

    feat(frontend): 点击编辑设置聚焦输入框

--- site/src/components/InputField.vue
+++ site/src/components/InputField.vue
@@ -1,15 +1,15 @@
 <template>
-  <Space v-if="isEditing">
-    <Input v-model="valueNow" :max-length="maxLength" />
+  <Space v-show="isEditing">
+    <Input ref="inputRef" v-model="valueNow" :max-length="maxLength" />
     <div class="input-field-opt">
       <Button type="primary" @click="onConfirm">保存</Button>
       <Button @click="onCancle">取消</Button>
     </div>
   </Space>
-  <Space v-else>
+  <Space v-show="!isEditing">
     <TypographyText>{{ valueNow }}</TypographyText>
     <button class="icon-link" title="更改此栏" @click="startEdit">
-      <IconEdit />
+      <IconEdit :size="18" />
     </button>
   </Space>
 </template>
@@ -35,6 +35,7 @@ const props = withDefaults(defineProps<InputFieldProps>(), {
 })
 const valueNow = ref<string>(props.inputValue)
 const isEditing = ref<boolean>(false)
+const inputRef = ref<InstanceType<typeof Input>>()
 const onConfirm = () => {
   if (valueNow.value.trim() === '' || valueNow.value === props.inputValue) {
     valueNow.value = props.inputValue
@@ -48,6 +49,10 @@ const onCancle = () => {
 }
 const startEdit = () => {
   isEditing.value = true
+  const timer = setTimeout(() => {
+    inputRef.value!.inputRef!.focus()
+    clearTimeout(timer)
+  }, 0)
 }
 
 watch(
