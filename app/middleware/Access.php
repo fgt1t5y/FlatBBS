9ec7ee2e4da43d351d1dec607f3311c9de519b89
望 <1501034130@qq.com>
2023-10-15 19:01:49 +0800

    chore(backend): 移除后端跨域

--- app/middleware/Access.php
+++ app/middleware/Access.php
@@ -11,17 +11,18 @@ class Access implements MiddlewareInterface
 {
     public function process(Request $request, callable $handle): Response
     {
-        $response = $request->method() == 'OPTIONS' ? response('') : $handle($request);
-        if ($request->method() === 'GET') {
-            return $response;
-        }
-        $response->withHeaders([
-            'Access-Control-Allow-Origin' =>            $request->header('Origin', '*'),
-            'Access-Control-Allow-Credentials' =>       'true',
-            'Content-Type' =>                           'application/json;charset=UTF-8',
-            'Access-Control-Allow-Methods' =>           '*',
-            'Access-Control-Allow-Headers' =>           'Content-Type,XFILENAME,XFILECATEGORY,XFILESIZE'
-        ]);
-        return $response;
+        // $response = $request->method() == 'OPTIONS' ? response('') : $handle($request);
+        return $response = $request->method() == 'OPTIONS' ? response('') : $handle($request);
+        //     if ($request->method() === 'GET') {
+        //         return $response;
+        //     }
+        //     $response->withHeaders([
+        //         'Access-Control-Allow-Origin' =>            $request->header('Origin', '*'),
+        //         'Access-Control-Allow-Credentials' =>       'true',
+        //         'Content-Type' =>                           'application/json;charset=UTF-8',
+        //         'Access-Control-Allow-Methods' =>           '*',
+        //         'Access-Control-Allow-Headers' =>           'Content-Type,XFILENAME,XFILECATEGORY,XFILESIZE'
+        //     ]);
+        //     return $response;
     }
 }
