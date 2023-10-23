8faade19e1c1548b18bee52ed9c88a6d92533251
望 <1501034130@qq.com>
2023-10-23 19:59:50 +0800

    feat(backend): 优化接口分页

--- app/controller/TopicController.php
+++ app/controller/TopicController.php
@@ -9,15 +9,17 @@ class TopicController
 {
     public function list(Request $request)
     {
+        $last_id = (int) $request->post('last', 0);
         $_limit = (int) $request->post('limit');
 
         if ($_limit > 20) {
             return json_message(STATUS_BAD_REQUEST, '');
         }
 
-        $topics = Topic::orderBy('created_at')
+        $topics = Topic::orderBy('last_reply_at')
             ->limit(max($_limit, 10))
             ->with(['author:id,username', 'last_reply:id,username'])
+            ->where('id', '>', $last_id)
             ->get(['id', 'title', 'author_id', 'last_reply_id', 'reply_count']);
 
         return json_message(0, '完成', $topics);
