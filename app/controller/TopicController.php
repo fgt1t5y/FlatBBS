5fbf5a15fe878635b020adac2f610be551a6d7dd
望 <1501034130@qq.com>
2023-10-29 21:59:36 +0800

    feat(backend): 按版块查询帖子

--- app/controller/TopicController.php
+++ app/controller/TopicController.php
@@ -11,28 +11,33 @@ class TopicController
     {
         $last_id = (int) $request->post('last', 0);
         $_limit = (int) $request->post('limit');
+        $board = (int) $request->post('board');
 
         if ($_limit > 20 || $last_id < 0) {
             return json_message(STATUS_BAD_REQUEST, '参数错误');
         }
 
-        $topics = Topic::orderBy('last_reply_at')
+        $builder = Topic::orderBy('last_reply_at')
             ->limit(max($_limit, 10))
             ->with([
                 'author:id,username',
                 'board:id,name,color',
             ])
-            ->where('id', '>', $last_id, 'and', '')
-            ->get([
-                'id',
-                'title',
-                'author_id',
-                'last_reply_id',
-                'board_id',
-                'reply_count',
-                'last_reply_at'
-            ]);
+            ->where('id', '>', $last_id);
 
-        return json_message(STATUS_OK, '完成', $topics);
+        if ($board) {
+            $builder = $builder->where('board_id', $board);
+        }
+
+        $result = $builder->get([
+            'id',
+            'title',
+            'author_id',
+            'board_id',
+            'reply_count',
+            'last_reply_at'
+        ]);
+
+        return json_message(STATUS_OK, '完成', $result);
     }
 }
