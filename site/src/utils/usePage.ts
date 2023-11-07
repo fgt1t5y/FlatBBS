3507d069795b76f1550b49e38340f0992320c504
望 <1501034130@qq.com>
2023-11-07 21:03:37 +0800

    chore: 使用全局router示例而非useRouter()获取

--- site/src/utils/usePage.ts
+++ site/src/utils/usePage.ts
@@ -1,8 +1,7 @@
+import router from '@/router';
 import { computed } from 'vue';
-import { useRouter } from 'vue-router';
 
 export const usePage = () => {
-  const router = useRouter();
   const path = computed(() => {
     return router.currentRoute.value.fullPath;
   });
