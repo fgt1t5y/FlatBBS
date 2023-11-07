e7b2ab91584a218e7a9aab488ed84ee3e0f76ee0
望 <1501034130@qq.com>
2023-11-07 20:20:53 +0800

    chore: 移除配色方案中的无用变量

--- site/src/App.vue
+++ site/src/App.vue
@@ -4,14 +4,16 @@
     :theme-overrides="overrideTheme"
     :locale="zhCN"
   >
-    <main id="flatbbs">
-      <NDialogProvider>
-        <NMessageProvider>
-          <RouterView />
-          <NGlobalStyle />
-        </NMessageProvider>
-      </NDialogProvider>
-    </main>
+    <NThemeEditor>
+      <main id="flatbbs">
+        <NDialogProvider>
+          <NMessageProvider>
+            <RouterView />
+            <NGlobalStyle />
+          </NMessageProvider>
+        </NDialogProvider>
+      </main>
+    </NThemeEditor>
   </NConfigProvider>
 </template>
 
@@ -26,6 +28,7 @@ import {
   darkTheme,
   NGlobalStyle,
   zhCN,
+  NThemeEditor,
 } from 'naive-ui'
 
 const user = useUserStore()
