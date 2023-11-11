4a4f6655680b8c99472803558d9c6243505a956c
望 <1501034130@qq.com>
2023-11-11 19:34:12 +0800

    feat: 完成话题编辑器UI

---
+++ site/src/components/TopicEditor.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="topic-editor">
+    <NInput
+      v-model:value="topicTitle"
+      :minlength="minLength"
+      :maxlength="64"
+      placeholder="话题标题..."
+      show-count
+      clearable
+      @focus="onTitleInputFocus"
+      @input="onTitleInputChange"
+    />
+    <NInput
+      v-if="isShowFull"
+      ref="mainInputRef"
+      type="textarea"
+      placeholder="在此输入话题正文，支持 Markdown（选填）"
+      :rows="3"
+    />
+    <div v-if="isShowFull" class="topic-editor-opt">
+      <div class="topic-editor-tool">
+        <NButton
+          title="收起编辑器"
+          secondary
+          circle
+          @click="isShowFull = false"
+        >
+          <template #icon>
+            <ChevronUpIcon size="18px" />
+          </template>
+        </NButton>
+        <NButton title="插入 Emoji" secondary circle>
+          <template #icon>
+            <SmileIcon size="18px" />
+          </template>
+        </NButton>
+        <NButton title="插入图片到文末" secondary circle>
+          <template #icon>
+            <ImageIcon size="18px" />
+          </template>
+        </NButton>
+      </div>
+      <div class="topic-editor-submit">
+        <NButton
+          title="发布话题"
+          type="primary"
+          :disabled="disabledSubmitButton"
+          round
+        >
+          发布
+        </NButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import '@/style/TopicEditor.css'
+import { NInput, NButton, type InputInst } from 'naive-ui'
+import { SmileIcon, ImageIcon, ChevronUpIcon } from 'tdesign-icons-vue-next'
+import { ref } from 'vue'
+
+defineOptions({
+  name: 'TopicEditor',
+})
+
+const minLength = 5
+const topicTitle = ref<string>('')
+const mainInputRef = ref<InputInst>()
+const disabledSubmitButton = ref<boolean>(true)
+// 是否展开完整编辑器
+const isShowFull = ref<boolean>(false)
+const onTitleInputFocus = () => {
+  isShowFull.value = true
+}
+const onTitleInputChange = () => {
+  if (topicTitle.value.length >= minLength) {
+    disabledSubmitButton.value = false
+  } else {
+    disabledSubmitButton.value = true
+  }
+}
+</script>
