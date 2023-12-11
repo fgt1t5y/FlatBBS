5a4dabdbbb076e49d4a1fe83eb2bf3ee75af95c2
望 <1501034130@qq.com>
2023-12-11 19:51:12 +0800

    fix: 404页面布局异常

--- site/src/views/NotFoundView.vue
+++ site/src/views/NotFoundView.vue
@@ -1,15 +1,18 @@
 <template>
-  <NSpace vertical align="center" class="error-page" style="margin-top: 5rem">
-    <div></div>
-    <NH2 align-text>
-      <NText type="error">页面不存在</NText>
-    </NH2>
-    <NText>这里似乎什么都没有</NText>
-    <NButton type="error" round @click="page.goHome(false)">首页</NButton>
-  </NSpace>
+  <MainContent>
+    <NSpace vertical align="center" class="error-page" style="margin-top: 5rem">
+      <div></div>
+      <NH2 align-text>
+        <NText type="error">页面不存在</NText>
+      </NH2>
+      <NText>这里似乎什么都没有</NText>
+      <NButton type="error" round @click="page.goHome(false)">首页</NButton>
+    </NSpace>
+  </MainContent>
 </template>
 
 <script setup lang="ts">
+import MainContent from '@/components/MainContent.vue'
 import { NH2, NText, NSpace, NButton } from 'naive-ui'
 import { usePage } from '@/utils'
 
