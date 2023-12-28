7cd1e2cf69c0067d8c25a98dc32384d3909902fa
望 <1501034130@qq.com>
2023-12-28 22:33:07 +0800

    fix: 移动端过渡动画背景问题

--- site/src/views/RootView.vue
+++ site/src/views/RootView.vue
@@ -64,7 +64,7 @@
     </template>
     <template #content>
       <RouterView v-slot="{ Component }">
-        <Transition :name="route.meta.transition" :css="false">
+        <Transition :name="route.meta.transition" :css="!isDesktop">
           <KeepAlive :max="10">
             <component :is="Component" />
           </KeepAlive>
