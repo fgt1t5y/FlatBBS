b53102b754618cde63c822de2a7e193a2436b779
望 <1501034130@qq.com>
2024-03-19 22:47:14 +0800

    chore: 导出service所有文件

--- site/src/services/index.ts
+++ site/src/services/index.ts
@@ -1,5 +1,7 @@
 export * from './auth';
 export * from './boards';
+export * from './discussions';
 export * from './file';
+export * from './instance';
 export * from './topics';
 export * from './user';
