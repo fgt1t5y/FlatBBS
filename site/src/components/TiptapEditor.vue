f9dc73f5d0c52ddb375d78d8a78937bc758cfd8c
望 <1501034130@qq.com>
2024-05-08 21:38:20 +0800

    refactor(editor): 使用Tiptap代替wangEditor

---
+++ site/src/components/TiptapEditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="editor">
+    <EditorContent :editor="editor" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import '@/style/RichEditor.css'
+import { EditorContent, useEditor } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import Placeholder from '@tiptap/extension-placeholder'
+import { onBeforeUnmount } from 'vue'
+
+const editor = useEditor({
+  content: '',
+  extensions: [
+    StarterKit.configure({
+      heading: {
+        levels: [1, 2],
+      },
+    }),
+    Placeholder.configure({
+      placeholder: '点击输入内容',
+    }),
+  ],
+})
+
+onBeforeUnmount(() => {
+  if (editor.value) {
+    editor.value.destroy()
+  }
+})
+</script>
--- site/src/views/BoardPublishView.vue
+++ site/src/views/BoardPublishView.vue
@@ -1,23 +1,12 @@
 <template>
-  <MainContent>
+  <MainContent disable-panels>
     <PageTitle title="发布" />
-    <div>
-      <EditorToolbar :editor="editorRef" />
-      <Editor @created="onEditorCreated" />
-    </div>
+    <TiptapEditor />
   </MainContent>
 </template>
 
 <script setup lang="ts">
-import Editor from '@/components/Editor.vue'
-import EditorToolbar from '@/components/EditorToolbar.vue'
 import MainContent from '@/components/MainContent.vue'
 import PageTitle from '@/components/PageTitle.vue'
-import { shallowRef } from 'vue'
-import type { IDomEditor } from '@wangeditor/editor'
-
-const editorRef = shallowRef()
-const onEditorCreated = (editor: IDomEditor) => {
-  editorRef.value = editor
-}
+import TiptapEditor from '@/components/TiptapEditor.vue'
 </script>
