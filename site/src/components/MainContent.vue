8ef3ac8bd3a6fe67116abb285157391499cb4d05
望 <1501034130@qq.com>
2024-05-08 21:37:40 +0800

    feat(layout): 页面右侧栏可禁用隐藏

--- site/src/components/MainContent.vue
+++ site/src/components/MainContent.vue
@@ -3,18 +3,9 @@
     <div class="grid-main-content">
       <slot />
     </div>
-    <div v-if="isDesktop" class="grid-main-panels">
+    <div v-if="isDesktop && !disablePanels" class="grid-main-panels">
       <div class="grid-main-panels-inner">
         <slot name="panels" />
-        <footer>
-          <NText :depth="3">
-            此站点由
-            <NA href="https://github.com/fgt1t5y/FlatBBS" target="_blank">
-              FlatBBS
-            </NA>
-            驱动
-          </NText>
-        </footer>
       </div>
     </div>
   </div>
@@ -22,9 +13,16 @@
 
 <script setup lang="ts">
 import { isDesktop } from '@/utils'
-import { NText, NA } from 'naive-ui'
 
 defineOptions({
   name: 'MainContent',
+})
+
+interface MainContentProps {
+  disablePanels?: boolean
+}
+
+const props = withDefaults(defineProps<MainContentProps>(), {
+  disablePanels: false,
 })
 </script>
