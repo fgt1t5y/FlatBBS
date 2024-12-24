391bb6d913f710651e5fb2f1380b98aa6d32a7be
望 <1501034130@qq.com>
2024-12-24 16:08:29 +0800

    dev: 修改数据库名称

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => 3306,
-            'database' => 'flat_bbs',
+            'database' => 'flatbbs',
             'username' => 'root',
             'password' => 'lyghj456',
             'charset' => 'utf8mb4',
