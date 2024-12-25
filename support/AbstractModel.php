f7e6d510d8f8a8e6c21731760f0b2d24fe985498
望 <1501034130@qq.com>
2024-12-25 11:25:08 +0800

    chore: 移除残留的无用代码

--- support/AbstractModel.php
+++ support/AbstractModel.php
@@ -17,31 +17,5 @@ class AbstractModel extends Model
     protected static function boot(): void
     {
         parent::boot();
-
-        static::saving(function (self $model) {
-            EventManager::dispatchModelEvent(static::class, 'saving', $model);
-        });
-
-        static::saved(function (self $model) {
-            EventManager::dispatchModelEvent(static::class, 'saved', $model);
-        });
-
-        static::deleting(function (self $model) {
-            EventManager::dispatchModelEvent(static::class, 'deleting', $model);
-        });
-
-        static::deleted(function (self $model) {
-            EventManager::dispatchModelEvent(static::class, 'deleted', $model);
-        });
-    }
-
-    public static function listen(string $event, callable $callback): void
-    {
-        EventManager::addModelListener(static::class, $event, $callback);
-    }
-
-    public static function dispatch(string $event, $model)
-    {
-        EventManager::dispatchModelEvent(static::class, $event, $model);
     }
 }
