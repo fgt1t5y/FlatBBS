809267c23374157174f2e41c8e5b25b9b86c1ef1
望 <1501034130@qq.com>
2024-12-26 22:42:45 +0800

    feat: 实现FocusTrap、modal使用FocusTrap

--- site/src/components/Modal.vue
+++ site/src/components/Modal.vue
@@ -4,24 +4,26 @@
       <div v-show="visible" class="modal">
         <div class="h-full w-full bg-indigo-900 opacity-50"></div>
         <div class="modal-wrapper">
-          <div
-            ref="modalInnerRef"
-            class="modal-inner"
-            :style="{
-              'min-width': minWidth,
-              'min-height': minHeight,
-            }"
-          >
-            <div class="p-3 border-bt flex justify-between">
-              <span class="text-base font-bold">{{ title }}</span>
-              <button v-if="closeButton" @click="closeModal">
-                <X class="size-5" />
-              </button>
+          <FocusTrap :active="enableFocusTrap">
+            <div
+              ref="modalInnerRef"
+              class="modal-inner"
+              :style="{
+                'min-width': minWidth,
+                'min-height': minHeight,
+              }"
+            >
+              <div class="p-3 border-bt flex justify-between">
+                <span class="text-base font-bold">{{ title }}</span>
+                <button v-if="closeButton" @click="closeModal">
+                  <X class="size-5" />
+                </button>
+              </div>
+              <div class="p-3">
+                <slot />
+              </div>
             </div>
-            <div class="p-3">
-              <slot />
-            </div>
-          </div>
+          </FocusTrap>
         </div>
       </div>
     </Transition>
@@ -29,15 +31,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { nextTick, ref, watch } from 'vue'
 import { onClickOutside } from '@vueuse/core'
 import { X } from '@vicons/tabler'
+import { FocusTrap } from './FocusTrap'
 
 defineOptions({
   name: 'Modal',
 })
 
-defineProps<{
+const props = defineProps<{
   title: string
   minWidth?: string
   minHeight?: string
@@ -52,6 +55,7 @@ const emits = defineEmits<{
 const modalInnerRef = ref<HTMLElement>()
 const mount = defineModel<boolean>('mount')
 const visible = defineModel<boolean>('visible')
+const enableFocusTrap = ref<boolean>(false)
 
 const closeModal = () => {
   visible.value = false
@@ -68,8 +72,12 @@ watch(
   (v) => {
     if (v) {
       emits('show')
+      nextTick(() => {
+        enableFocusTrap.value = true
+      })
     } else {
       emits('hide')
+      enableFocusTrap.value = false
     }
   },
 )
--- site/src/utils/helper.ts
+++ site/src/utils/helper.ts
@@ -60,3 +60,86 @@ export const isMobile = breakpoint.smaller('sm');
 export const isPad = breakpoint.greater('sm');
 export const isDesktop = breakpoint.greater('lg');
 export const isDev = import.meta.env.DEV;
+
+export function resolveTo(
+  selector: string | (() => HTMLElement | null),
+): HTMLElement | null {
+  if (typeof selector === 'string') {
+    return document.querySelector(selector);
+  }
+  return selector();
+}
+
+function isHTMLElement(node: Node): node is HTMLElement {
+  return node instanceof HTMLElement;
+}
+
+function isFocusable(element: HTMLElement): boolean {
+  if (
+    element.tabIndex > 0 ||
+    (element.tabIndex === 0 && element.getAttribute('tabIndex') !== null)
+  ) {
+    return true;
+  }
+
+  if (element.getAttribute('disabled')) {
+    return false;
+  }
+
+  switch (element.nodeName) {
+    case 'A':
+      return (
+        !!(element as HTMLAnchorElement).href &&
+        (element as HTMLAnchorElement).rel !== 'ignore'
+      );
+    case 'INPUT':
+      return (
+        (element as HTMLInputElement).type !== 'hidden' &&
+        (element as HTMLInputElement).type !== 'file'
+      );
+    case 'BUTTON':
+    case 'SELECT':
+    case 'TEXTAREA':
+      return true;
+    default:
+      return false;
+  }
+}
+
+function attemptFocus(element: HTMLElement): boolean {
+  if (!isFocusable(element)) {
+    return false;
+  }
+  try {
+    element.focus({ preventScroll: true });
+    // eslint-disable-next-line no-empty
+  } catch (e) {}
+  return document.activeElement === element;
+}
+
+export function focusFirstDescendant(node: Node): boolean {
+  for (let i = 0; i < node.childNodes.length; i++) {
+    const child = node.childNodes[i];
+    if (isHTMLElement(child)) {
+      if (attemptFocus(child) || focusFirstDescendant(child)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+export function focusLastDescendant(element: Node): boolean {
+  for (let i = element.childNodes.length - 1; i >= 0; i--) {
+    const child = element.childNodes[i];
+    if (isHTMLElement(child)) {
+      if (attemptFocus(child) || focusLastDescendant(child)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// eslint-disable-next-line prefer-const
+export let focusTrapStack: string[] = []
