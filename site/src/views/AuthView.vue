483fb19ddf2adeb52a5625c5bd0f323814c77915
望 <1501034130@qq.com>
2025-02-21 19:41:00 +0800

    refactor: 使用请求钩子简化登录请求逻辑

--- site/src/views/AuthView.vue
+++ site/src/views/AuthView.vue
@@ -5,8 +5,8 @@
       <CommonForm
         :form="inputForm"
         :submit-label="$t('action.login')"
-        :disabled="isDealing"
-        @submit="actionLogin"
+        :disabled="loading"
+        @submit="handleLogin"
       >
         <FormInput
           v-model="inputForm.email"
@@ -38,24 +38,23 @@ import MainContent from '@/components/MainContent.vue'
 import PageTitle from '@/components/PageTitle.vue'
 import CommonForm from '@/components/CommonForm.vue'
 import FormInput from '@/components/form/FormInput.vue'
+import { useRequest } from 'alova/client'
 
-const isDealing = ref<boolean>(false)
 const inputForm = ref({
   email: '',
   password: '',
 })
+
 const user = useUserStore()
 const route = useRoute()
 
-const actionLogin = () => {
-  isDealing.value = true
-  login(inputForm.value.email, inputForm.value.password)
-    .then(async () => {
-      await user.fetch()
-      router.replace({ path: (route.query.next as string) || '/' })
-    })
-    .finally(() => {
-      isDealing.value = false
-    })
-}
+const { loading, send: handleLogin } = useRequest(
+  () => login(inputForm.value.email, inputForm.value.password),
+  {
+    immediate: false,
+  },
+).onSuccess(async () => {
+  await user.fetch()
+  router.replace({ path: (route.query.next as string) || '/' })
+})
 </script>
