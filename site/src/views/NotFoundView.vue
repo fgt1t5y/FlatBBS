acc03d33e89573ef3765067ccb1e333ec67878d2
望 <1501034130@qq.com>
2025-02-26 21:43:00 +0800

    chore: 为404页面增加页面header

--- site/src/views/NotFoundView.vue
+++ site/src/views/NotFoundView.vue
@@ -1,5 +1,6 @@
 <template>
   <MainContent disable-panels :title="$t('page.not_found')">
+    <PageTitle :title="$t('page.not_found')" />
     <div class="flex flex-col items-center gap-8 my-6">
       <h2 class="text-3xl">{{ $t('page.not_found') }}</h2>
       <button
@@ -14,4 +15,5 @@
 
 <script setup lang="ts">
 import MainContent from '@/components/MainContent.vue'
+import PageTitle from '@/components/PageTitle.vue';
 </script>
